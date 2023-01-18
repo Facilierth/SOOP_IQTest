cb9452882b50adf1e3b7b9d7150116cd3f7e28cd
Facilierth <katri.jaroszynska@gmail.com>
2023-01-18 15:48:10 +0100

    Added Title Frame

--- src/Main.java
+++ src/Main.java
@@ -21,11 +21,58 @@ public class Main extends JPanel {
         // DrawingFunction drawing = new DrawingFunction();
         // drawing.creatingFrame();
 
+        JPanel panel = new JPanel();
+
+        JFrame titleFrame = new JFrame("Raven's matrices");
+        titleFrame.setResizable(false);
+        titleFrame.setSize(PositionsOfShapes.frameSizeX, PositionsOfShapes.frameSizeY);
+        titleFrame.setVisible(true);
+        titleFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel title = new JLabel("Raven's Matrices");
+
+        JLabel subtext = new JLabel("Click anywhere to continue");
+
+        title.setBounds( 0, (int) (PositionsOfShapes.frameSizeY * 0.33), (int) (PositionsOfShapes.frameSizeX), (int) (PositionsOfShapes.frameSizeY*0.33));
+        title.setText("Raven's Matrices");
+
+        title.setHorizontalAlignment(SwingConstants.CENTER);
+        title.setFont(new Font("Calibri", Font.BOLD, 80));
+
+        subtext.setFont(new Font("Calibre", Font.ITALIC, 15));
+        subtext.setBounds(0, (int) (PositionsOfShapes.frameSizeY * 0.4),(int) (PositionsOfShapes.frameSizeX), (int) (PositionsOfShapes.frameSizeY*0.33));
+        subtext.setHorizontalAlignment(SwingConstants.CENTER);
+
+
+
+        JButton button = new JButton();
+        button.setContentAreaFilled(false);
+        button.setBorderPainted(false);
+
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                SetUp();
+                titleFrame.setVisible(false);
+            }
+
+        });
+        titleFrame.getContentPane().add(subtext);
+        titleFrame.getContentPane().add(title);
+
+        titleFrame.add(button);
+
+    }
+
+    private static void SetUp(){
         PositionsOfButtons.settingButtonPosition();
         RandomArray.creatingRandomArrays();
 
         textArea = new JTextArea();
         textArea.setBounds((int) (0.33 * PositionsOfShapes.frameSizeX), (int) (0.9 * PositionsOfShapes.frameSizeY), (int) (0.33 * PositionsOfShapes.frameSizeX), 34);
+
+        Color backgroundColor = new Color(0,0,0);
+        Color color =  (new Color(162, 225, 165));
+
         textArea.setBackground(new Color(162, 225, 165));
         textArea.setCaretColor(new Color(162, 225, 165));
         textArea.setFont(new Font("Comic Sans", Font.PLAIN, 25));
